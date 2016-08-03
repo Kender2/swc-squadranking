59d036363831f04bdcd4e6850776c37a0873e89b
Kender <info@squadsofdeath.com>
2016-08-03 19:40:35 +0200

    improve header bar for mobile

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,23 +1,8 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target=".navbar-collapse" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">Home</a>
-        </div>
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-left">
-                <li class="{{Route::currentRouteNamed('squadranking') ? 'active' : ''}}"><a
-                            href="{{ route('squadranking') }}">Squad ranking</a></li>
-                <li class="{{Route::currentRouteNamed('faq') ? 'active' : ''}}"><a
-                            href="{{ route('faq') }}">F.A.Q.</a></li>
-            </ul>
-            <form class="navbar-form navbar-right" role="search" method="get"
+            <a class="navbar-brand" href="{{ route('squadranking') }}">Squad ranking</a>
+            <form class="navbar-form navbar-left" role="search" method="get"
                   action="{{ action('SquadController@squadSearch') }}">
                 <div class="input-group">
                     <input class="form-control" type="text" name="q" value="{{ request('q') }}"
