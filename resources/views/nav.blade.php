ebc46f8a9c251ff60a9e88b8adc772dbc2697eb1
Sergey Pariev <spariev@gmail.com>
2016-12-30 16:41:29 +0200

    #12 search with select from http://www.bootply.com/gWOb4RcY86 WIP

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -3,28 +3,24 @@
         <div class="navbar-header">
             <a class="navbar-brand" href="{{ route('squadranking') }}">Squad ranking</a>
             <form class="navbar-form navbar-left" role="search" method="get"
-                  action="{{ action('SquadController@squadSearch') }}">
+                  action="{{ action('SquadController@search') }}">
                 <div class="input-group col-xs-6 col-sm-10 col-lg-12">
-                    <input class="form-control input" type="search" name="q" value="{{ request('q') }}"
-                           placeholder="Search squad">
+		    <div class="ddl-select input-group-btn">
+                    	<select id="searchtype" class="selectpicker form-control" data-style="btn-primary">
+                  	    <option value="" data-hidden="true" class="type-title">SEARCH</option>
+                  	    <option value="squad">Squad</option>
+                  	    <option value="player">Player</option>
+			</select>
+                    </div>
+		    /* ^ http://www.bootply.com/gWOb4RcY86 */
+              	    <input class="form-control input" type="search" name="q" value="{{ request('q') }}"
+                           placeholder="Enter here">
                     <span class="input-group-btn">
                         <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i>
                         </button>
                     </span>
                 </div>
             </form>
-            <form class="navbar-form navbar-right" role="search" method="get"
-                  action="{{ action('SquadController@commanderSearch') }}">
-                <div class="input-group col-xs-6 col-sm-10 col-lg-12">
-                    <input class="form-control input" type="search" name="sq" value="{{ request('sq') }}"
-                           placeholder="Search commander">
-                    <span class="input-group-btn">
-                        <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i>
-                        </button>
-                    </span>
-                </div>
-            </form>
-
 	  </div>
     </div>
 </nav>
