6491217fbc7824ab99a0f928a16f58e8af80b400
Kender <info@squadsofdeath.com>
2017-04-01 09:19:16 +0200

    Adjust sigma multiplier to increase certainty.

--- config/sod.php
+++ config/sod.php
@@ -4,6 +4,6 @@ return [
     'game_url' => env('SOD_GAME_URL', 'https://n7-starts-web-active.playdom.com'),
     'cms_url' => env('SOD_CMS_URL', 'https://starts0.content.disney.io/cloud-cms/'),
     'win_threshold' => 10,
-    'sigma_multiplier' => 2,
+  'sigma_multiplier' => 4,
     'main_page_url' => 'http://squadsofdeath.wixsite.com/squadsofdeath',
 ];
